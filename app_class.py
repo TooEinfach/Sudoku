098660ac3bda397aaf051164f31d313b66c54f50
Nick Stenquist <whiteshadow148@gmail.com>
2020-08-13 15:00:16 -0600

    Updated box so now you can select squares

--- app_class.py
+++ app_class.py
@@ -7,6 +7,8 @@ class App:
         self.window = pygame.display.set_mode((WIDTH, HEIGHT))
         self.running = True
         self.grid = testBoard
+        self.selected = None
+        self.mousePos = None
 
     def run(self):
         while self.running:
@@ -20,14 +22,40 @@ class App:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                selected = self.mouseOnGrid()
+                if selected:
+                    self.selected = selected
+                else:
+                    print("not on board")
+                    self.selected = None
 
     def update(self):
-        pass
+        self.mousePos = pygame.mouse.get_pos()
 
     def draw(self):
         self.window.fill(WHITE)
+        if self.selected:
+            self.drawselection(self.window, self.selected)
         self.drawGrid(self.window)
         pygame.display.update()
 
+    def drawselection(self, window, pos):
+         pygame.draw.rect(window, LIGHTBLUE, ((pos[0]*cellSize)+gridPos[0], (pos[1]*cellSize)+gridPos[1], cellSize, cellSize))
+
     def drawGrid(self, window):
-        pygame.draw.rect(window, BLACK, (gridPos[0], gridPos[1], WIDTH-120, HEIGHT-120), 2)
+        pygame.draw.rect(window, BLACK, (gridPos[0], gridPos[1], WIDTH-150, HEIGHT-150), 2)
+        for x in range(9):
+            if x % 3 != 0:
+                pygame.draw.line(window, BLACK, (gridPos[0]+(x*cellSize), gridPos[1]), (gridPos[0]+(x*cellSize), gridPos[1]+450))
+                pygame.draw.line(window, BLACK, (gridPos[0], gridPos[1]+(x*cellSize)), (gridPos[0]+450, gridPos[1]+(x*cellSize)))
+            else:
+                pygame.draw.line(window, BLACK, (gridPos[0]+(x*cellSize), gridPos[1]), (gridPos[0]+(x*cellSize), gridPos[1]+450), 2)
+                pygame.draw.line(window, BLACK, (gridPos[0], gridPos[1]+(x*cellSize)), (gridPos[0]+450, gridPos[1]+(x*cellSize)), 2)
+
+    def mouseOnGrid(self):
+        if self.mousePos[0] < gridPos[0] or self.mousePos[1] < gridPos[1]:
+            return False
+        if self.mousePos[0] > gridPos[0]+gridSize or self.mousePos[1] > gridPos[1]+gridSize:
+            return False
+        return ((self.mousePos[0]-gridPos[0])//cellSize, (self.mousePos[1]-gridPos[1])//cellSize)
--- settings.py
+++ settings.py
@@ -4,9 +4,12 @@ HEIGHT = 600
 # Colours
 WHITE = (255,255,255)
 BLACK = (0,0,0)
+LIGHTBLUE = (96, 216, 232)
 
 # Boards
 testBoard = [[0 for x in range(9)] for x in range(9)]
 
 # Positions and sizes
-gridPos = (60,100)
+gridPos = (75,100)
+cellSize = 50
+gridSize = cellSize*9
