b45794ce5e4c0962ee6a3d09514fc4c1f6ce6134
Nick Stenquist <whiteshadow148@gmail.com>
2020-08-17 11:22:30 -0600

    Web scrapping to get boards
    video9 tutorial

--- app_class.py
+++ app_class.py
@@ -1,4 +1,6 @@
 import pygame, sys
+import requests
+from bs4 import BeautifulSoup
 from settings import * 
 from buttonClass import *
 
@@ -8,6 +10,7 @@ class App:
         self.window = pygame.display.set_mode((WIDTH, HEIGHT))
         self.running = True
         self.grid = finishedBoard
+        # self.grid = self.getPuzzle("1")
         self.selected = None
         self.mousePos = None
         self.state = "playing"
@@ -20,6 +23,9 @@ class App:
         self.incorrectCells = []
         self.font = pygame.font.SysFont("arial", cellSize//2)
         self.load()
+
+        board = self.getPuzzle("1")
+        print(board)
 
     def run(self):
         while self.running:
@@ -151,6 +157,29 @@ class App:
                                 self.incorrectCells.append([xidx, k])
 
 #### Helper Fucntions ####
+
+    def getPuzzle(self, difficulty):
+        # Difficulty needs to be passed in as string as digit 1-4
+        html_doc = requests.get("http://www.websudoku.com/?level={}".format(difficulty)).content
+        soup = BeautifulSoup(html_doc)
+        ids = ['f00', 'f01', 'f02', 'f03', 'f04', 'f05', 'f06', 'f07', 'f08', 'f10', 'f11', 
+        'f12', 'f13', 'f14', 'f15', 'f16', 'f17', 'f18', 'f20', 'f21', 'f22', 'f23', 
+        'f24', 'f25', 'f26', 'f27', 'f28', 'f30', 'f31', 'f32', 'f33', 'f34', 'f35', 
+        'f36', 'f37', 'f38', 'f40', 'f41', 'f42', 'f43', 'f44', 'f45', 'f46', 'f47', 
+        'f48', 'f50', 'f51', 'f52', 'f53', 'f54', 'f55', 'f56', 'f57', 'f58', 'f60', 
+        'f61', 'f62', 'f63', 'f64', 'f65', 'f66', 'f67', 'f68', 'f70', 'f71', 'f72', 
+        'f73', 'f74', 'f75', 'f76', 'f77', 'f78', 'f80', 'f81', 'f82', 'f83', 'f84', 
+        'f85', 'f86', 'f87', 'f88']
+        data = []
+        for cid in ids:
+            data.append(soup.find('input', id=cid))
+        board = [[0 for x in range(9)] for x in range(9)]
+        for index, cell enumerate(data):
+            try:
+                board[index//9][index%9] = int(cell['value'])
+            except:
+                pass
+        return data
 
     def shadeIncorrectCells(self, window, incorrect):
         for cell in incorrect:
